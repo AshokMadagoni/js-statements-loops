f05c3b01db9d56deaa83bde076b3c6a6f4c61f91
AshokMadagoni <madagoniashok1@gmail.com>
2024-09-30 22:54:23 +0530

    added

---
+++ task.js
@@ -0,0 +1,77 @@
+// 1.Answer
+
+for(var a=1;a<=10;a++){
+    console.log(a);  
+}
+
+var a=1;
+while(a<=10){
+    console.log(a);
+    a++  
+}
+
+var a=1;
+do{
+    console.log(a);
+    a++
+}
+while(a<=10)
+
+// 2.Answer
+
+for(var a=2;a<=10;a=a+2){
+    console.log(a);
+    
+}
+
+var a=2;
+while(a<=10){
+    console.log(a);
+    a=a+2;
+}
+
+var a=2;
+do{
+    console.log(a);
+    a=a+2;    
+}
+while(a<=10);
+
+// 3.Answer
+
+for(var a=1;a<=10;a=a+2){
+    console.log(a);
+}
+
+var a=1;
+while(a<=10){
+    console.log(a);
+    a=a+2;
+}
+
+var a=1;
+do{
+    console.log(a);
+    a=a+2;
+}
+while(a<=10);
+
+// 4.Answer
+
+var a=40;
+if(a>80){
+    console.log("grade A");
+}
+else if(a>60){
+    console.log("grade B");
+}
+else if(a>50){
+    console.log("ghrade c");
+}
+else if(a>35 && a<45){
+    console.log(a); 
+    console.log("grade D");   
+}
+else{
+    console.log("fail");   
+}
